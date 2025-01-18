7000e4c7a3c6d4ec5bd05a74ced4ab816ac2e658
Dmytro <dmytrostoianov05@gmail.com>
2025-01-18 21:08:45 +0100

    Add files upload functionality
    Make files to be uploaded via system dialog and drag-n-drop.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,19 +1,84 @@
+import { ChangeEvent, useState } from 'react'
+
 const Home = () => {
+    const [files, setFiles] = useState<File[]>([])
+
+    const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
+        if (e.target.files) {
+            setFiles(Array.from(e.target.files))
+        }
+    }
+
+    const handleDrop = (e: React.DragEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        if (e.dataTransfer.files) {
+            setFiles(Array.from(e.dataTransfer.files))
+        }
+    }
+
+    const handleDragOver = (e: React.DragEvent<HTMLFormElement>) => {
+        e.preventDefault()
+    }
+
     return (
-        <main className="mt-[10%] flex flex-col items-center justify-end gap-[3rem]">
+        <main className="mx-10 mt-[15%] flex flex-col items-center justify-end gap-[3rem] lg:mt-[10%]">
             <div className="flex items-end gap-[1rem]">
-                <img className="w-[3.5rem]" src="logo.png" alt="logo" />
-                <h1 className="font-comfortaa text-5xl">EventifyAI</h1>
-            </div>
-            <button className="flex h-[12rem] w-[32rem] flex-col items-center justify-center gap-[0.75rem] border-2 border-black bg-white">
                 <img
-                    className="mt-[1rem] w-[4rem]"
-                    src="upload.png"
-                    alt="upload"
+                    className="w-[3rem] lg:w-[3.5rem]"
+                    src="logo.png"
+                    alt="logo"
                 />
-                <span className="font-comfortaa text-sm text-black">
+                <h1 className="font-comfortaa text-4xl lg:text-5xl">
+                    EventifyAI
+                </h1>
+            </div>
+            <form
+                onDrop={handleDrop}
+                onDragOver={handleDragOver}
+                className="relative flex h-[10rem] w-full flex-col items-center gap-[0.75rem] border-2 border-black bg-white md:w-[32rem] lg:h-[12rem]"
+            >
+                {files.length > 0 ? (
+                    <div className="h-[6.5rem] w-full overflow-y-auto border-gray-300 lg:h-[8.5rem]">
+                        {files.map((f, index) => (
+                            <div
+                                key={index}
+                                className="flex items-center justify-between border-b-2 border-gray-100 p-2"
+                            >
+                                <span>{f.name}</span>
+                                <span className="text-sm text-gray-500">
+                                    {Math.round(f.size / 1024)} KB
+                                </span>
+                            </div>
+                        ))}
+                    </div>
+                ) : (
+                    <img
+                        className="mt-[3rem] w-[4rem] lg:mt-[5rem]"
+                        src="upload.png"
+                        alt="upload"
+                    />
+                )}
+
+                <label
+                    htmlFor="file-input"
+                    className="font-comfortaa cursor-pointer text-center text-sm text-black"
+                >
                     Drop files here or click to upload
-                </span>
+                </label>
+                <input
+                    id="file-input"
+                    type="file"
+                    accept="image/png, image/jpeg, image/jpg"
+                    multiple
+                    onChange={handleFileChange}
+                    className={`absolute left-0 top-0 h-full w-full cursor-pointer opacity-0 ${files.length > 0 ? 'z-[-1]' : 'z-[100]'}`}
+                />
+            </form>
+            <button
+                className={`rounded-lg border-2 border-black px-4 py-2 ${files.length === 0 && 'cursor-not-allowed'}`}
+                disabled={files.length === 0}
+            >
+                Create event link
             </button>
         </main>
     )
