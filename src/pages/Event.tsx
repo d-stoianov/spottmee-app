7746b683f3accd35fba7edc686bf928fcc48781c
Dmytro <dmytrostoianov05@gmail.com>
2025-01-19 02:11:09 +0100

    Implement files download with jszip

--- src/pages/Event.tsx
+++ src/pages/Event.tsx
@@ -1,9 +1,11 @@
 import PageLayout from '@/layout/PageLayout'
 import { EventifyService } from '@/service'
+import JSZip from 'jszip'
 import { useEffect, useState } from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
 
 const service = new EventifyService()
+const zip = new JSZip()
 
 const EventPage = () => {
     const { id: eventId } = useParams()
@@ -15,7 +17,7 @@ const EventPage = () => {
     useEffect(() => {
         async function getImages() {
             if (!eventId) {
-                navigate("/")
+                navigate('/')
                 return
             }
 
@@ -32,22 +34,65 @@ const EventPage = () => {
         getImages()
     }, [])
 
+    const downloadAllImages = async () => {
+        try {
+            const promises = imagesPath.map(async (img) => {
+                const response = await fetch(img)
+                const blob = await response.blob()
+
+                const fileName = img.split('/').pop()
+                if (fileName) {
+                    zip.file(fileName, blob)
+                }
+            })
+
+            await Promise.all(promises)
+
+            const zipData = await zip.generateAsync({
+                type: 'blob',
+                streamFiles: true,
+            })
+            const link = document.createElement('a')
+            link.href = window.URL.createObjectURL(zipData)
+            link.download = `eventify-ai-${eventId}.zip`
+            link.click()
+        } catch (error) {
+            console.error('error while downloading images:', error)
+        }
+    }
+
     return (
         <PageLayout>
             {isLoading ? (
                 <span>Loading...</span>
             ) : (
-                <section className="grid grid-cols-4 gap-4">
-                    {imagesPath.map((img, idx) => (
-                        <div
-                            className="flex flex-col items-center justify-end gap-2"
-                            key={idx}
-                        >
-                            <img className="w-[20rem] rounded-lg" src={img} alt="image" />
-                            <span>{EventifyService.getHostedImageFileName(img)}</span>
-                        </div>
-                    ))}
-                </section>
+                <>
+                    <section className="grid grid-cols-4 gap-4">
+                        {imagesPath.map((img, idx) => (
+                            <div
+                                className="flex flex-col items-center justify-end gap-2"
+                                key={idx}
+                            >
+                                <img
+                                    className="w-[20rem] rounded-lg"
+                                    src={img}
+                                    alt="image"
+                                />
+                                <span>
+                                    {EventifyService.getHostedImageFileName(
+                                        img
+                                    )}
+                                </span>
+                            </div>
+                        ))}
+                    </section>
+                    <button
+                        className="rounded-lg border-2 px-4 py-2"
+                        onClick={downloadAllImages}
+                    >
+                        Download all
+                    </button>
+                </>
             )}
         </PageLayout>
     )
