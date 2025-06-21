85b73c1d712546890601acecd696b7eae894a50c
Dmytro Stoianov <dmytrostoianov05@gmail.com>
2025-06-21 13:00:02 +0300

    Setup i18n for localization

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,6 +1,7 @@
 import { AuthProvider } from '@/providers/AuthProvider'
 import { AppProvider } from './provider'
 import AppRouter from './router'
+import '@/config/i18n'
 
 const App: React.FC = () => {
     return (
