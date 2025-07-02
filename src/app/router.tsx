9e19088dedbb89cca5c02327be4344413eb7cc3b
Dmytro Stoianov <dmytrostoianov05@gmail.com>
2025-07-03 00:49:21 +0200

    Create PublicRoute to redirect users to the protected routes

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -1,18 +1,27 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
+import ProtectedRoute from './routes/ProtectedRoute'
+import PublicRoute from './routes/PublicRoute'
+
 import SignInRoute from './routes/auth/sign-in'
 import SignUpRoute from './routes/auth/sign-up'
+
+import DashboardLayout from '@/components/layout/DashboardLayout'
 
 import HomeRoute from './routes/app'
 import EventRoute from './routes/app/event/event-id'
 import CompareRoute from './routes/app/event/compare/compare-id'
-import ProtectedRoute from './routes/ProtectedRoute'
-import DashboardLayout from '@/components/layout/DashboardLayout'
 
 const createAppRouter = () => {
     return createBrowserRouter([
-        { path: '/sign-in', Component: SignInRoute },
-        { path: '/sign-up', Component: SignUpRoute },
+        {
+            element: <PublicRoute />,
+            children: [
+                { path: '/sign-in', Component: SignInRoute },
+                { path: '/sign-up', Component: SignUpRoute },
+            ],
+        },
+
         // protected routes
         {
             element: <ProtectedRoute />,
