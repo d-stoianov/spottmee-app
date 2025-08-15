5fd42b444da6b685a8e1330d24a42d4abf235557
Dmytro Stoianov <dmytrostoianov05@gmail.com>
2025-08-15 19:41:26 +0200

    Create header

--- src/app/routes/app/index.tsx
+++ src/app/routes/app/index.tsx
@@ -1,16 +1,10 @@
 import Main from '@/components/layout/Main'
 import { Typography } from '@/components/ui/Typography'
-import { useAuth } from '@/providers/AuthProvider'
 
 const HomeRoute: React.FC = () => {
-    const { user, signOut } = useAuth()
-
     return (
         <Main>
             <Typography variant="heading2">Home</Typography>
-            <Typography variant="bodyDefault">{user?.email}</Typography>
-
-            <button onClick={() => signOut()}>logout</button>
         </Main>
     )
 }
--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,14 +1,45 @@
 import clsx from 'clsx'
 import { twMerge } from 'tailwind-merge'
+import logo from '@/assets/logo.svg'
+import { useAuth } from '@/providers/AuthProvider'
+import { Typography } from '@/components/ui/Typography'
 
 type HeaderProps = React.HTMLAttributes<HTMLElement>
 
 const Header: React.FC<HeaderProps> = ({ className, ...props }) => {
+    const { user, signOut } = useAuth()
+
     return (
         <header
-            className={twMerge(clsx('h-[4rem] w-full bg-base', className))}
+            className={twMerge(
+                clsx(
+                    'flex h-[4rem] w-full items-center justify-between bg-base px-[4rem]',
+                    className
+                )
+            )}
             {...props}
-        />
+        >
+            <img className="mt-[0.5rem] h-[2.5rem]" src={logo} />
+
+            {user && (
+                <div className="flex items-center gap-2">
+                    <Typography className="text-white" variant="bodyDefault">
+                        {user?.email}
+                    </Typography>
+                    <button
+                        className="w-fit"
+                        onClick={() => signOut()}
+                    >
+                        <Typography
+                            className="text-white"
+                            variant="bodyDefault"
+                        >
+                            logout
+                        </Typography>
+                    </button>
+                </div>
+            )}
+        </header>
     )
 }
 
